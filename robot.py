136f435d42de49913d7cdb48ac6ccb508e7af647
thedimi44s <good9997@gmail.com>
2017-09-28 00:17:23 +0300

    add new code to robot.py

--- robot.py
+++ robot.py
@@ -1,45 +1,70 @@
-import requests
+import requests  
+import datetime
 
-offset = 0 # параметр необходим для подтверждения обновления
-URL = 'https://api.telegram.org/bot' # URL на который отправляется запрос
-TOKEN = '454989610:AAG_ls5ewAzhziRD2lAZR4Zo_zBOQ3wJIjw' # токен вашего бота, полученный от @BotFather
-data = {'offset': offset  1, 'limit': 0, 'timeout': 0}
+class BotHandler:
 
-try: # обрабатываем исключения
-    request = requests.post(URL  TOKEN  '/getUpdates', data=data) # собственно сам запрос
-except:
-    print('Error getting updates')
-    return False
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
-if not request.status_code == 200: return False # проверяем пришедший статус ответа
-if not request.json()['ok']: return False
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
-for update in request.json()['result']:
-    offset = update['update_id'] #  подтверждаем текущее обновление
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
 
-    if 'message' not in update or 'text' not in update['message']: # это просто текст или какая-нибудь картиночка?
-        print('Unknown message')
-        continue
+    def get_last_update(self):
+        get_result = self.get_updates()
 
-    message_data = { # формируем информацию для отправки сообщения
-        'chat_id': update['message']['chat']['id'], # куда отправляем сообщение
-        'text': "I'm <b>bot</b>", # само сообщение для отправки
-        'reply_to_message_id': update['message']['message_id'], # если параметр указан, то бот отправит сообщение в reply
-        'parse_mode': 'HTML' # про форматирование текста ниже
-    }
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
 
-    try:
-        request = requests.post(URL  TOKEN  '/sendMessage', data=message_data) # запрос на отправку сообщения
-    except:
-        print('Send message error')
-        return False
-
-    if not request.status_code == 200: # проверим статус пришедшего ответа
-        return False
+        return last_update
 		
-		while True:
+greet_bot = BotHandler(token)  
+greetings = ('hello', 'hi', 'greetings', 'sup')  
+now = datetime.datetime.now()
+
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':  
     try:
-        check_updates()
-    except KeyboardInterrupt: # порождается, если бота остановил пользователь
-        print('Interrupted by the user')
-        break
+        main()
+    except KeyboardInterrupt:
+        exit()
